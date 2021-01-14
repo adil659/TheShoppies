eb0c6993a76b20a6889134e751acdd6e812334bb
Adil <aughr063@uottawa.ca>
2021-01-14 16:25:24 -0500

    updates request to https

--- src/requests/omdb.js
+++ src/requests/omdb.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const base_url = 'http://www.omdbapi.com/'
+const base_url = 'https://www.omdbapi.com/'
 const api_key = '44b49dbd'
 const searchMovie = async (searchTerm) => {
     console.log('called')
